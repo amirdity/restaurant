2b07fad5445dd55f2715263f154b962bbfc9ba32
amir fattahi <amirfattahi22@gmail.com>
2024-06-21 17:22:53 +0330

    Add files via upload

--- Provide.jsx
+++ Provide.jsx
@@ -9,35 +9,24 @@ function cartReducer(state, action) {
   if (action.type === "ADD") {
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
-    // in this phase , i am adding a new item to the items array
-    // now i should check the id of items is repetitive or not
-    //by useing findIndex() i will know the ansewr
-    // if the answer was -1 it means that id is not repeated
-    // and returns undifiend
-
-    // action is not added yet we are checking is id repeated or not ?
+    // conCat will create a new array
+    // const existingCartItemIndex = state.items.findIndex()
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
-    // if the id is exist , now i have access to that object inside
-    //of that array and this is an object
-    const existingCartItem = state.items[existingCartItemIndex];
     let updatedItems;
+    const existingCartItem = state.items[existingCartItemIndex];
     if (existingCartItem) {
       const updatedItem = {
-        // object
         ...existingCartItem,
-        amount: existingCartItem.amount + action.item.amount,
+        amount: state.items[existingCartItemIndex].amount + action.item.amount,
       };
-      // this will be triged updateing correctly
-      // widoth it ... it will be duplicated
       updatedItems = [...state.items];
-
-      updatedItems[existingCartItemIndex] = updatedItem;
+      state.items[existingCartItemIndex] = updatedItem;
     } else {
-      // conCat will create a new array
       updatedItems = state.items.concat(action.item);
     }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
@@ -64,11 +53,14 @@ function CartProvider({ children }) {
     addItem: addItemCartHandler,
     removeItem: removeItemFromCartHandler,
   };
+
   return (
     <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
   );
 }
+
 CartProvider.propTypes = {
   children: PropTypes.array,
 };
+
 export default CartProvider;
